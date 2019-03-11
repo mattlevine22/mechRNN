fix normalization treatments, improve run script, make more+better plots

run_script.py
@@ -8,113 +8,180 @@ yb = 100
 c_gamma = 0.05
 sim_model_params = (yb, c_gamma)
 rnn_model_params = (yb, c_gamma)
-rnn_BAD_model_params = (yb, 0.9*c_gamma)
 
 sim_model = exp_decay_model
 rnn_sim_model = easy_exp_decay_model
 
 n_sims = 3
-n_epochs = 100
+n_epochs = 30
 
 train_frac = 0.6
 for i in range(n_sims):
+	all_dirs = []
+
 	np.random.seed()
 
 	# master output directory name
-	output_dir = 'output/experiment2/sim' + str(i+1)
+	output_dir = 'output/experiment3/sim' + str(i+1)
 	# simulate clean and noisy data
-	input_data, y_clean, y_noisy, normz_info_clean, normz_info_noisy = make_RNN_data(
+	input_data, y_clean, y_noisy = make_RNN_data(
 	              sim_model, tspan, sim_model_params, noise_frac=0.1, output_dir=output_dir)
 
-
-	# do train/test split
+	###### do train/test split #######
 	n_train = int(np.floor(train_frac*len(y_clean)))
 	y_clean_train = y_clean[:n_train]
 	y_clean_test = y_clean[n_train:]
 	y_noisy_train = y_noisy[:n_train]
 	y_noisy_test = y_noisy[n_train:]
-	x_train = input_data[:,:n_train]
-	x_test = input_data[:,n_train:]
+	x_train = input_data[:, :n_train]
+	x_test = input_data[:, n_train:]
 
-	all_dirs = []
+	####### collect normalization information from TRAINING SET ONLY ######
+	normz_info_clean = {}
+	normz_info_clean['Ymax'] = max(y_clean_train)
+	normz_info_clean['Ymin'] = min(y_clean_train)
+	# normz_info_clean['Ymean'] = np.mean(y_clean_train)
+	# normz_info_clean['Ysd'] = np.std(y_clean_train)
+	normz_info_clean['Xmean'] = np.mean(x_train)
+	normz_info_clean['Xsd'] = np.std(x_train)
+
+	normz_info_noisy = {}
+	normz_info_noisy['Ymax'] = max(y_noisy_train)
+	normz_info_noisy['Ymin'] = min(y_noisy_train)
+	# normz_info_noisy['Ymean'] = np.mean(y_noisy_train)
+	# normz_info_noisy['Ysd'] = np.std(y_noisy_train)
+	normz_info_noisy['Xmean'] = np.mean(x_train)
+	normz_info_noisy['Xsd'] = np.std(x_train)
+
+	###### normalize TRAINING data #######
+	# # y (MIN/MAX [0,1])
+	# y_clean_train = (y_clean_train - normz_info_clean['Ymin'])/(normz_info_clean['Ymax'] - normz_info_clean['Ymin'])
+	# y_noisy_train = (y_noisy_train - normz_info_noisy['Ymin'])/(normz_info_noisy['Ymax'] - normz_info_noisy['Ymin'])
+	# # x (MIN/MAX [0,1])
+	# # y_clean_train = (y_clean_train - normz_info_clean['Ymean']) / normz_info_clean['Ysd']
+	# # y_noisy_train = (y_noisy_train - normz_info_noisy['Ymean']) / normz_info_noisy['Ysd']
+	x_train = (x_train - normz_info_clean['Xmean']) / normz_info_clean['Xsd']
+
+	# ###### normalize TESTING data ########
+	# # y (MIN/MAX [0,1])
+	# y_clean_test = (y_clean_test - normz_info_clean['Ymin'])/(normz_info_clean['Ymax'] - normz_info_clean['Ymin'])
+	# y_noisy_test = (y_noisy_test - normz_info_noisy['Ymin'])/(normz_info_noisy['Ymax'] - normz_info_noisy['Ymin'])
+	# x (MIN/MAX [0,1])
+	# y_clean_test = (y_clean_test - normz_info_clean['Ymean']) / normz_info_clean['Ysd']
+	# y_noisy_test = (y_noisy_test - normz_info_noisy['Ymean']) / normz_info_noisy['Ysd']
+
+	x_test = (x_test - normz_info_clean['Xmean']) / normz_info_clean['Xsd']
+
+	########## NOW start running RNN fits ############
+
 	#### run vanilla RNN ####
 	forward = forward_vanilla
 	hidden_size = 6
+
 	# train on clean data
+	normz_info = normz_info_clean
 	run_output_dir = output_dir + '/vanillaRNN_clean'
 	all_dirs.append(run_output_dir)
 	torch.manual_seed(0)
+	y_clean_train_norm = (y_clean_train - normz_info['Ymin'])/(normz_info['Ymax'] - normz_info['Ymin'])
+	y_noisy_train_norm = (y_noisy_train - normz_info['Ymin'])/(normz_info['Ymax'] - normz_info['Ymin'])
+	y_clean_test_norm = (y_clean_test - normz_info['Ymin'])/(normz_info['Ymax'] - normz_info['Ymin'])
+	y_noisy_test_norm = (y_noisy_test - normz_info['Ymin'])/(normz_info['Ymax'] - normz_info['Ymin'])
 	train_RNN(forward,
-      y_clean_train, y_clean_train, x_train,
-      y_clean_test, y_noisy_test, x_test,
+      y_clean_train_norm, y_clean_train_norm, x_train,
+      y_clean_test_norm, y_noisy_test_norm, x_test,
       rnn_model_params, hidden_size, n_epochs, lr,
       run_output_dir, normz_info_clean, rnn_sim_model)
 
 	# train on noisy data
+	normz_info = normz_info_noisy
 	run_output_dir = output_dir + '/vanillaRNN_noisy'
 	all_dirs.append(run_output_dir)
 	torch.manual_seed(0)
-	# train_RNN(forward, input_data, tspan, y_clean, y_noisy,
-	  # rnn_model_params, hidden_size, n_epochs, lr, run_output_dir, normz_info_noisy, rnn_sim_model)
+	y_clean_train_norm = (y_clean_train - normz_info['Ymin'])/(normz_info['Ymax'] - normz_info['Ymin'])
+	y_noisy_train_norm = (y_noisy_train - normz_info['Ymin'])/(normz_info['Ymax'] - normz_info['Ymin'])
+	y_clean_test_norm = (y_clean_test - normz_info['Ymin'])/(normz_info['Ymax'] - normz_info['Ymin'])
+	y_noisy_test_norm = (y_noisy_test - normz_info['Ymin'])/(normz_info['Ymax'] - normz_info['Ymin'])
 	train_RNN(forward,
-      y_clean_train, y_noisy_train, x_train,
-      y_clean_test, y_noisy_test, x_test,
+      y_clean_train_norm, y_noisy_train_norm, x_train,
+      y_clean_test_norm, y_noisy_test_norm, x_test,
       rnn_model_params, hidden_size, n_epochs, lr,
-      run_output_dir, normz_info_noisy, rnn_sim_model)
+      run_output_dir, normz_info, rnn_sim_model)
 
 
 	#### run mechRNN ###
 	forward = forward_mech
 	hidden_size = 7
+
 	# train on clean data
+	normz_info = normz_info_clean
 	run_output_dir = output_dir + '/mechRNN_clean'
 	all_dirs.append(run_output_dir)
 	torch.manual_seed(0)
-	# train_RNN(forward, input_data, tspan, y_clean, y_clean,
-	#   rnn_model_params, hidden_size, n_epochs, lr, run_output_dir, normz_info_clean, rnn_sim_model)
+	y_clean_train_norm = (y_clean_train - normz_info['Ymin'])/(normz_info['Ymax'] - normz_info['Ymin'])
+	y_noisy_train_norm = (y_noisy_train - normz_info['Ymin'])/(normz_info['Ymax'] - normz_info['Ymin'])
+	y_clean_test_norm = (y_clean_test - normz_info['Ymin'])/(normz_info['Ymax'] - normz_info['Ymin'])
+	y_noisy_test_norm = (y_noisy_test - normz_info['Ymin'])/(normz_info['Ymax'] - normz_info['Ymin'])
 	train_RNN(forward,
-      y_clean_train, y_clean_train, x_train,
-      y_clean_test, y_noisy_test, x_test,
+      y_clean_train_norm, y_clean_train_norm, x_train,
+      y_clean_test_norm, y_noisy_test_norm, x_test,
       rnn_model_params, hidden_size, n_epochs, lr,
       run_output_dir, normz_info_clean, rnn_sim_model)
+
 	# train on noisy data
+	normz_info = normz_info_noisy
 	run_output_dir = output_dir + '/mechRNN_noisy'
 	all_dirs.append(run_output_dir)
 	torch.manual_seed(0)
-	# train_RNN(forward, input_data, tspan, y_clean, y_noisy,
-	#   rnn_model_params, hidden_size, n_epochs, lr, run_output_dir, normz_info_noisy, rnn_sim_model)
+	y_clean_train_norm = (y_clean_train - normz_info['Ymin'])/(normz_info['Ymax'] - normz_info['Ymin'])
+	y_noisy_train_norm = (y_noisy_train - normz_info['Ymin'])/(normz_info['Ymax'] - normz_info['Ymin'])
+	y_clean_test_norm = (y_clean_test - normz_info['Ymin'])/(normz_info['Ymax'] - normz_info['Ymin'])
+	y_noisy_test_norm = (y_noisy_test - normz_info['Ymin'])/(normz_info['Ymax'] - normz_info['Ymin'])
 	train_RNN(forward,
-      y_clean_train, y_noisy_train, x_train,
-      y_clean_test, y_noisy_test, x_test,
+      y_clean_train_norm, y_noisy_train_norm, x_train,
+      y_clean_test_norm, y_noisy_test_norm, x_test,
       rnn_model_params, hidden_size, n_epochs, lr,
       run_output_dir, normz_info_noisy, rnn_sim_model)
 
 	#### run mechRNN w/ BAD parameter ###
 	forward = forward_mech
 	hidden_size = 7
-	# train on clean data
-	run_output_dir = output_dir + '/mechRNN_badParam_clean'
-	all_dirs.append(run_output_dir)
-	torch.manual_seed(0)
-	# train_RNN(forward, input_data, tspan, y_clean, y_clean,
-	#   rnn_BAD_model_params, hidden_size, n_epochs, lr, run_output_dir, normz_info_clean, rnn_sim_model)
-	train_RNN(forward,
-      y_clean_train, y_clean_train, x_train,
-      y_clean_test, y_noisy_test, x_test,
-      rnn_BAD_model_params, hidden_size, n_epochs, lr,
-      run_output_dir, normz_info_clean, rnn_sim_model)
-	# train on noisy data
-	run_output_dir = output_dir + '/mechRNN_badParam_noisy'
-	all_dirs.append(run_output_dir)
-	torch.manual_seed(0)
-	# train_RNN(forward, input_data, tspan, y_clean, y_noisy,
-	#   rnn_BAD_model_params, hidden_size, n_epochs, lr, run_output_dir, normz_info_noisy, rnn_sim_model)
-	train_RNN(forward,
-      y_clean_train, y_noisy_train, x_train,
-      y_clean_test, y_noisy_test, x_test,
-      rnn_BAD_model_params, hidden_size, n_epochs, lr,
-      run_output_dir, normz_info_noisy, rnn_sim_model)
+
+
+	for badness in [0.01, 0.1, 0.5, 0.9, 0.99]:
+		rnn_BAD_model_params = (yb, badness*c_gamma)
+
+		# train on clean data
+		normz_info = normz_info_clean
+		run_output_dir = output_dir + '/mechRNN_badGamma{}_clean'.format(badness)
+		all_dirs.append(run_output_dir)
+		torch.manual_seed(0)
+		y_clean_train_norm = (y_clean_train - normz_info['Ymin'])/(normz_info['Ymax'] - normz_info['Ymin'])
+		y_noisy_train_norm = (y_noisy_train - normz_info['Ymin'])/(normz_info['Ymax'] - normz_info['Ymin'])
+		y_clean_test_norm = (y_clean_test - normz_info['Ymin'])/(normz_info['Ymax'] - normz_info['Ymin'])
+		y_noisy_test_norm = (y_noisy_test - normz_info['Ymin'])/(normz_info['Ymax'] - normz_info['Ymin'])
+		train_RNN(forward,
+	      y_clean_train_norm, y_clean_train_norm, x_train,
+	      y_clean_test_norm, y_noisy_test_norm, x_test,
+	      rnn_BAD_model_params, hidden_size, n_epochs, lr,
+	      run_output_dir, normz_info_clean, rnn_sim_model)
+
+		# train on noisy data
+		normz_info = normz_info_noisy
+		run_output_dir = output_dir + '/mechRNN_badGamma{}_noisy'.format(badness)
+		all_dirs.append(run_output_dir)
+		torch.manual_seed(0)
+		y_clean_train_norm = (y_clean_train - normz_info['Ymin'])/(normz_info['Ymax'] - normz_info['Ymin'])
+		y_noisy_train_norm = (y_noisy_train - normz_info['Ymin'])/(normz_info['Ymax'] - normz_info['Ymin'])
+		y_clean_test_norm = (y_clean_test - normz_info['Ymin'])/(normz_info['Ymax'] - normz_info['Ymin'])
+		y_noisy_test_norm = (y_noisy_test - normz_info['Ymin'])/(normz_info['Ymax'] - normz_info['Ymin'])
+		train_RNN(forward,
+	      y_clean_train_norm, y_noisy_train_norm, x_train,
+	      y_clean_test_norm, y_noisy_test_norm, x_test,
+	      rnn_BAD_model_params, hidden_size, n_epochs, lr,
+	      run_output_dir, normz_info_noisy, rnn_sim_model)
 
 	# plot comparative training errors
-	compare_fits(all_dirs, output_dir)
+	compare_fits([d for d in all_dirs if "clean" in d], output_fname=output_dir+'/model_comparisons_clean')
+	compare_fits([d for d in all_dirs if "noisy" in d], output_fname=output_dir+'/model_comparisons_noisy')
 

utils.py
@@ -20,13 +20,11 @@ import pdb
 ## 1. Simulate ODE
 # function that returns dy/dt
 def exp_decay_model(y,t,yb,c_gamma,x):
-  # pdb.set_trace()
   x_t = x[np.where(x[:,0] <= t)[0][-1], 1]
   dydt = -c_gamma*(y-yb) + x_t
   return dydt
 
 def easy_exp_decay_model(y_in,t,yb,c_gamma,x_in):
-  # pdb.set_trace()
   dydt = -c_gamma*(y_in-yb) + x_in[0]
   return dydt
 
@@ -44,26 +42,6 @@ def run_ode_model(model, tspan, sim_model_params, tau=50, noise_frac=0, output_d
   y_clean = odeint(model, y0, tspan, args=my_args)
 
   y_noisy = y_clean + noise_frac*np.mean(y_clean)*np.random.randn(len(y_clean),1)
-
-  # collect normalization information
-  normz_info_clean = {}
-  normz_info_clean['Ymax'] = max(y_clean)
-  normz_info_clean['Ymin'] = min(y_clean)
-  normz_info_clean['Xmean'] = np.mean(x[:,1])
-  normz_info_clean['Xsd'] = np.std(x[:,1])
-
-  normz_info_noisy = {}
-  normz_info_noisy['Ymax'] = max(y_noisy)
-  normz_info_noisy['Ymin'] = min(y_noisy)
-  normz_info_noisy['Xmean'] = np.mean(x[:,1])
-  normz_info_noisy['Xsd'] = np.std(x[:,1])
-
-  #normalize y (MIN/MAX [0,1])
-  y_clean = (y_clean - normz_info_clean['Ymin'])/(normz_info_clean['Ymax'] - normz_info_clean['Ymin'])
-  y_noisy = (y_noisy - normz_info_noisy['Ymin'])/(normz_info_noisy['Ymax'] - normz_info_noisy['Ymin'])
-
-  #normalize x (MIN/MAX [0,1])
-  x[:,1] = (x[:,1] - normz_info_noisy['Xmean']) / normz_info_noisy['Xsd']
 
   ## 2. Plot ODE
   fig, ax1 = plt.subplots()
@@ -87,7 +65,7 @@ def run_ode_model(model, tspan, sim_model_params, tau=50, noise_frac=0, output_d
   fig.savefig(fname=output_dir+'/driven_exponential_decay_simpleInputs')
   plt.close(fig)
 
-  return y_clean, y_noisy, x, normz_info_clean, normz_info_noisy
+  return y_clean, y_noisy, x
 
 
 def make_RNN_data(model, tspan, sim_model_params, noise_frac=0, output_dir="."):
@@ -95,7 +73,7 @@ def make_RNN_data(model, tspan, sim_model_params, noise_frac=0, output_dir="."):
   if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-  y_clean, y_noisy, x, normz_info_clean, normz_info_noisy  = run_ode_model(model, tspan, sim_model_params, noise_frac=noise_frac, output_dir=output_dir)
+  y_clean, y_noisy, x  = run_ode_model(model, tspan, sim_model_params, noise_frac=noise_frac, output_dir=output_dir)
 
   # little section to upsample the random, piecewise constant x(t) function
   z = np.zeros([len(tspan),2])
@@ -109,29 +87,28 @@ def make_RNN_data(model, tspan, sim_model_params, noise_frac=0, output_dir="."):
     z[i,1] = prev
   x0 = z[:,None,1] # add an extra dimension to the ode inputs so that it is a tensor for PyTorch
 
-  input_data = torch.FloatTensor(x0.T)
-  # input = torch.FloatTensor(np.concatenate((x0,y),axis=1).T)
-  # output_data = torch.FloatTensor(y_noisy)
+  input_data = x0.T
 
-  return input_data, y_clean, y_noisy, normz_info_clean, normz_info_noisy
+  return input_data, y_clean, y_noisy
 
 
 ### RNN fitting section
 def forward_vanilla(input, hidden_state, w1, w2, b, c, v, *args):
-  # pdb.set_trace()
   hidden_state = torch.relu(b + torch.mm(w2,input) + torch.mm(w1,hidden_state))
   out = c + torch.mm(v,hidden_state)
   return  (out, hidden_state)
 
 
 def forward_mech(input, hidden_state, w1, w2, b, c, v, normz_info, model, model_params):
-  # pdb.set_trace()
   # unnormalize
   ymin = normz_info['Ymin']
   ymax = normz_info['Ymax']
+  # ymean = normz_info['Ymean']
+  # ysd = normz_info['Ysd']
   xmean = normz_info['Xmean']
   xsd = normz_info['Xsd']
 
+  # y0 = ymean + hidden_state[0].detach().numpy()*ysd
   y0 = ymin + ( hidden_state[0].detach().numpy()*(ymax - ymin) )
   tspan = [0,0.5,1]
   driver = xmean + xsd*input.detach().numpy()
@@ -140,6 +117,7 @@ def forward_mech(input, hidden_state, w1, w2, b, c, v, normz_info, model, model_
 
   # renormalize
   hidden_state[0] = torch.from_numpy( (y_out[-1] - ymin) / (ymax - ymin) )
+  # hidden_state[0] = torch.from_numpy( (y_out[-1] - ymean) / ysd )
 
   hidden_state = torch.relu(b + torch.mm(w2,input) + torch.mm(w1,hidden_state))
   out = c + torch.mm(v,hidden_state)
@@ -156,6 +134,9 @@ def train_RNN(forward,
     os.makedirs(output_dir)
 
   print('Starting RNN training for: ', output_dir)
+
+  x_train = torch.FloatTensor(x_train)
+  x_test = torch.FloatTensor(x_test)
 
   output_train = torch.FloatTensor(y_noisy_train)
   output_clean_train = torch.FloatTensor(y_clean_train)
@@ -232,7 +213,7 @@ def train_RNN(forward,
   loss_vec_clean_train = np.zeros(n_epochs)
   loss_vec_test = np.zeros(n_epochs)
   loss_vec_clean_test = np.zeros(n_epochs)
-  for i in range(n_epochs):
+  for i_epoch in range(n_epochs):
     total_loss_train = 0
     total_loss_clean_train = 0
     total_loss_test = 0
@@ -262,8 +243,8 @@ def train_RNN(forward,
       v.grad.data.zero_()
 
       hidden_state = hidden_state.detach()
-    loss_vec_train[i] = total_loss_train
-    loss_vec_clean_train[i] = total_loss_clean_train
+    loss_vec_train[i_epoch] = total_loss_train
+    loss_vec_clean_train[i_epoch] = total_loss_clean_train
 
     hidden_state = Variable(torch.zeros((hidden_size, 1)).type(dtype), requires_grad=False)
     for j in range(test_seq_length):
@@ -274,15 +255,72 @@ def train_RNN(forward,
       total_loss_clean_test += (pred - target_clean).pow(2).sum()/2
 
       hidden_state = hidden_state.detach()
-    loss_vec_test[i] = total_loss_test
-    loss_vec_clean_test[i] = total_loss_clean_test
+    loss_vec_test[i_epoch] = total_loss_test
+    loss_vec_clean_test[i_epoch] = total_loss_clean_test
 
-    # print updates every 50 iterations or in 10% incrememnts
-    if i % int( max(50, np.ceil(n_epochs/10)) ) == 0:
-       print("Epoch: {}\nTraining Loss = {}\nTesting Loss = {}".format(
-            i,
+    # print updates every iteration or in 10% incrememnts
+    if i_epoch % int( max(1, np.ceil(n_epochs/10)) ) == 0:
+      print("Epoch: {}\nTraining Loss = {}\nTesting Loss = {}".format(
+            i_epoch,
             total_loss_train.data.item(),
             total_loss_test.data.item()))
+         # plot predictions vs truth
+      fig, (ax1, ax3) = plt.subplots(1, 2)
+
+      # first run and plot training fits
+      hidden_state = Variable(torch.zeros((hidden_size, 1)).type(dtype), requires_grad=False)
+      predictions = []
+      for i in range(train_seq_length):
+        (pred, hidden_state) = forward(x_train[:,i:i+1], hidden_state, w1, w2, b, c, v, normz_info, model, model_params)
+        hidden_state = hidden_state
+        predictions.append(pred.data.numpy().ravel()[0])
+
+      ax1.scatter(np.arange(len(y_noisy_train)), y_noisy_train, color='red', s=10, alpha=0.3, label='noisy data')
+      ax1.plot(y_clean_train, color='red', label='clean data')
+      ax1.plot(predictions, color='black', label='NN fit', linewidth=3, linestyle='--')
+      ax1.set_xlabel('time')
+      ax1.set_ylabel('y(t)', color='red')
+      ax1.tick_params(axis='y', labelcolor='red')
+
+      ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+      color = 'tab:blue'
+      ax2.set_ylabel('x(t)', color=color)  # we already handled the x-label with ax1
+      ax2.plot((x_train[0,:].numpy()), ':', color=color, linestyle='--', label='driver/input data')
+      ax2.tick_params(axis='y', labelcolor=color)
+
+      ax1.set_title('Training Fit')
+
+      # NOW, show testing fit
+      hidden_state = Variable(torch.zeros((hidden_size, 1)).type(dtype), requires_grad=False)
+      predictions = []
+      for i in range(test_seq_length):
+        (pred, hidden_state) = forward(x_test[:,i:i+1], hidden_state, w1, w2, b, c, v, normz_info, model, model_params)
+        hidden_state = hidden_state
+        predictions.append(pred.data.numpy().ravel()[0])
+
+      ax3.scatter(np.arange(len(y_noisy_test)), y_noisy_test, color='red', s=10, alpha=0.3, label='noisy data')
+      ax3.plot(y_clean_test, color='red', label='clean data')
+      ax3.plot(predictions, color='black', label='NN fit', linewidth=3, linestyle='--')
+      ax3.set_xlabel('time')
+      ax3.set_ylabel('y(t)', color='red')
+      ax3.tick_params(axis='y', labelcolor='red')
+
+      ax4 = ax3.twinx()  # instantiate a second axes that shares the same x-axis
+
+      color = 'tab:blue'
+      ax4.set_ylabel('x(t)', color=color)  # we already handled the x-label with ax1
+      ax4.plot((x_test[0,:].numpy()), ':', color=color, linestyle='--', label='driver/input data')
+      ax4.tick_params(axis='y', labelcolor=color)
+
+      ax3.set_title('Testing Fit')
+
+      ax3.legend()
+      ax2.legend()
+
+      fig.suptitle('RNN fit to Exponential Decay simulation--' + str(i_epoch) + 'training epochs')
+      fig.savefig(fname=output_dir+'/rnn_fit_ode_iterEpochs'+str(i_epoch))
+      plt.close(fig)
 
   ## save loss_vec
   np.savetxt(output_dir+'/loss_vec_train.txt',loss_vec_train)
@@ -362,18 +400,19 @@ def train_RNN(forward,
   plt.close(fig)
 
 
-def compare_fits(my_dirs, output_dir="."):
+def compare_fits(my_dirs, output_fname="./training_comparisons"):
 
   fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3,
     figsize = [10, 4],
     sharey=True, sharex=True)
   for d in my_dirs:
+      d_label = d.split("/")[-1].rstrip('_noisy').rstrip('_clean')
       x = np.loadtxt(d+"/loss_vec_train.txt")
-      ax1.plot(x, label=d.split("/")[-1])
+      ax1.plot(x, label=d_label)
       x = np.loadtxt(d+"/loss_vec_clean_test.txt")
-      ax2.plot(x, label=d.split("/")[-1])
+      ax2.plot(x, label=d_label)
       x = np.loadtxt(d+"/loss_vec_test.txt")
-      ax3.plot(x, label=d.split("/")[-1])
+      ax3.plot(x, label=d_label)
 
   ax1.set_xlabel('Epochs')
   ax1.set_ylabel('Error')
@@ -387,7 +426,7 @@ def compare_fits(my_dirs, output_dir="."):
   ax3.set_title('Test Error (on noisy data)')
 
   # fig.suptitle("Comparison of training efficacy (trained on noisy data)")
-  fig.savefig(fname=output_dir+'/training_comparisons')
+  fig.savefig(fname=output_fname)
   plt.close(fig)
 
 