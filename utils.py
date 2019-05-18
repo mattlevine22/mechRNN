3ba3f52bd31d8cc3823b5f04f4c2ebf7eb021478
Matt Levine <mattlevine22@gmail.com>
2019-05-17 18:25:24 -0700

    adding useful function for arg parsing

--- utils.py
+++ utils.py
@@ -22,6 +22,13 @@ import pdb
 
 ### ODE simulation section
 ## 1. Simulate ODE
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
 
 def oscillator_2d(Y,t,a,b,c):
 	(x,y) = Y
