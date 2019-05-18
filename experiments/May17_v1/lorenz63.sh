150bce89f66181ced5e6ce45381cea472db4be88
Matt Levine <mattlevine22@gmail.com>
2019-05-17 19:58:54 -0700

    make jobs exclusive

--- experiments/May17_v1/lorenz63.sh
+++ experiments/May17_v1/lorenz63.sh
@@ -14,8 +14,8 @@ module load cuda/9.0
 module load python/2.7.15-tf
 
 # Execute jobs in parallel
-srun -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE.py --savedir lorenz63/one_epoch_v1/ --epoch 1 --delta_t 0.01 --t_end 10000 --train_frac 0.9995 &
-srun -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE.py --savedir lorenz63/one_epoch_v2/ --epoch 1 --delta_t 0.1 --t_end 10000 --train_frac 0.9995 &
-srun -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE.py --savedir lorenz63/one_epoch_v3/ --epoch 1 --delta_t 0.1 --t_end 100000 --train_frac 0.99995 &
-srun -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE.py --savedir lorenz63/loop_epoch_v1/ --epoch 100 --delta_t 0.01 --t_end 100 --train_frac 0.9
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE.py --savedir lorenz63/one_epoch_v1/ --epoch 1 --delta_t 0.01 --t_end 10000 --train_frac 0.9995 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE.py --savedir lorenz63/one_epoch_v2/ --epoch 1 --delta_t 0.1 --t_end 10000 --train_frac 0.9995 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE.py --savedir lorenz63/one_epoch_v3/ --epoch 1 --delta_t 0.1 --t_end 100000 --train_frac 0.99995 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE.py --savedir lorenz63/loop_epoch_v1/ --epoch 100 --delta_t 0.01 --t_end 100 --train_frac 0.9
 wait
