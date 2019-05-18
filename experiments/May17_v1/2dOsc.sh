00a15cab54408c5f6aa731c3e84f9b8a7272789d
Matt Levine <mattlevine22@gmail.com>
2019-05-17 20:05:27 -0700

    more batch script updates

--- experiments/May17_v1/2dOsc.sh
+++ experiments/May17_v1/2dOsc.sh
@@ -7,6 +7,7 @@
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --mem-per-cpu=10G   # memory per CPU core
 #SBATCH -c 1 # 1 core per task
+#SBATCH --exclusive
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 module purge
@@ -14,8 +15,8 @@ module load cuda/9.0
 module load python/2.7.15-tf
 
 # Execute jobs in parallel
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_2dOscillator_SCRIPTSTYLE.py --savedir 2dOsc/one_epoch_v1/ --epoch 1 --delta_t 0.01 --t_end 100 --train_frac 0.8 &
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_2dOscillator_SCRIPTSTYLE.py --savedir 2dOsc/one_epoch_v2/ --epoch 1 --delta_t 0.1 --t_end 100 --train_frac 0.8 &
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_2dOscillator_SCRIPTSTYLE.py --savedir 2dOsc/one_epoch_v3/ --epoch 1 --delta_t 0.1 --t_end 1000 --train_frac 0.8 &
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_2dOscillator_SCRIPTSTYLE.py --savedir 2dOsc/loop_epoch_v1/ --epoch 100000 --delta_t 0.1 --t_end 10 --train_frac 0.8
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_2dOscillator_SCRIPTSTYLE.py --savedir 2dOsc/one_epoch_v1 --epoch 1 --delta_t 0.01 --t_end 100 --train_frac 0.8 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_2dOscillator_SCRIPTSTYLE.py --savedir 2dOsc/one_epoch_v2 --epoch 1 --delta_t 0.1 --t_end 100 --train_frac 0.8 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_2dOscillator_SCRIPTSTYLE.py --savedir 2dOsc/one_epoch_v3 --epoch 1 --delta_t 0.1 --t_end 1000 --train_frac 0.8 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_2dOscillator_SCRIPTSTYLE.py --savedir 2dOsc/loop_epoch_v1 --epoch 100000 --delta_t 0.1 --t_end 10 --train_frac 0.8
 wait
