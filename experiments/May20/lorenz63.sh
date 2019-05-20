24e64f6a214ff7fb49acce133b9a8e807f3ce54a
Matt Levine <mattlevine22@gmail.com>
2019-05-20 15:30:17 -0700

    give more memory to lorenz

--- experiments/May20/lorenz63.sh
+++ experiments/May20/lorenz63.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=24:00:00   # walltime
 #SBATCH --ntasks=6   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
-#SBATCH --mem-per-cpu=10G   # memory per CPU core
+#SBATCH --mem-per-cpu=25G   # memory per CPU core
 #SBATCH -c 1 # 1 core per task
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
