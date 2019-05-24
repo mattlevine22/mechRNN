1ee760c743eb162a68cee5767a21d3b81ddb3dac
Matt Levine <mattlevine22@gmail.com>
2019-05-24 12:08:22 -0700

    new lorenz run

--- experiments/May24/lorenz63.sh
+++ experiments/May24/lorenz63.sh
@@ -2,8 +2,8 @@
 
 #Submit this script with: sbatch train_submission
 
-#SBATCH --time=36:00:00   # walltime
-#SBATCH --ntasks=2   # number of processor cores (i.e. tasks)
+#SBATCH --time=48:00:00   # walltime
+#SBATCH --ntasks=3   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --mem-per-cpu=25G   # memory per CPU core
 #SBATCH -c 1 # 1 core per task
@@ -16,4 +16,5 @@ module load python/2.7.15-tf
 # Execute jobs in parallel
 srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_eps.py --savedir lorenz63/loop_10000epochs_eps --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
 srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hiddensizes.py --savedir lorenz63/loop_10000epochs_hs --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE.py --savedir lorenz63/loop_10000epochs_perturbTrivial --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
 wait
