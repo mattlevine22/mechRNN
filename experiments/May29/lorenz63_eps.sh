f3b9cd5f04a3eb0bd3de64fc96cd80c5eac1f4b1
Matt Levine <mattlevine22@gmail.com>
2019-05-29 19:33:14 -0700

    improve run names

---
+++ experiments/May29/lorenz63_eps.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#Submit this script with: sbatch train_submission
+
+#SBATCH --time=48:00:00   # walltime
+#SBATCH --ntasks=10   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --mem-per-cpu=20G   # memory per CPU core
+#SBATCH -c 1 # 1 core per task
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+module purge
+module load cuda/9.0
+module load python/2.7.15-tf
+
+# Execute jobs in parallel
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_eps.py --savedir lorenz63_loop_10000epochs_eps/iter0 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_eps.py --savedir lorenz63_loop_10000epochs_eps/iter1 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_eps.py --savedir lorenz63_loop_10000epochs_eps/iter2 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_eps.py --savedir lorenz63_loop_10000epochs_eps/iter3 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_eps.py --savedir lorenz63_loop_10000epochs_eps/iter4 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_eps.py --savedir lorenz63_loop_10000epochs_eps/iter5 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_eps.py --savedir lorenz63_loop_10000epochs_eps/iter6 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_eps.py --savedir lorenz63_loop_10000epochs_eps/iter7 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_eps.py --savedir lorenz63_loop_10000epochs_eps/iter8 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_eps.py --savedir lorenz63_loop_10000epochs_eps/iter9 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9
+wait
---
+++ experiments/May29/lorenz63_hs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#Submit this script with: sbatch train_submission
+
+#SBATCH --time=48:00:00   # walltime
+#SBATCH --ntasks=10   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --mem-per-cpu=20G   # memory per CPU core
+#SBATCH -c 1 # 1 core per task
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+module purge
+module load cuda/9.0
+module load python/2.7.15-tf
+
+# Execute jobs in parallel
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hs.py --savedir lorenz63_loop_10000epochs_hs/iter0 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hs.py --savedir lorenz63_loop_10000epochs_hs/iter1 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hs.py --savedir lorenz63_loop_10000epochs_hs/iter2 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hs.py --savedir lorenz63_loop_10000epochs_hs/iter3 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hs.py --savedir lorenz63_loop_10000epochs_hs/iter4 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hs.py --savedir lorenz63_loop_10000epochs_hs/iter5 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hs.py --savedir lorenz63_loop_10000epochs_hs/iter6 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hs.py --savedir lorenz63_loop_10000epochs_hs/iter7 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hs.py --savedir lorenz63_loop_10000epochs_hs/iter8 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hs.py --savedir lorenz63_loop_10000epochs_hs/iter9 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9
+wht
