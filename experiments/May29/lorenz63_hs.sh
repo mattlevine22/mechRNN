2f3a2c74c33238f25f18e4e3eb85536303c10c6d
Matt Levine <mattlevine22@gmail.com>
2019-05-29 21:14:54 -0700

    fixing script name

--- experiments/May29/lorenz63_hs.sh
+++ experiments/May29/lorenz63_hs.sh
@@ -14,14 +14,14 @@ module load cuda/9.0
 module load python/2.7.15-tf
 
 # Execute jobs in parallel
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hs.py --savedir lorenz63_loop_10000epochs_hs/iter0 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hs.py --savedir lorenz63_loop_10000epochs_hs/iter1 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hs.py --savedir lorenz63_loop_10000epochs_hs/iter2 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hs.py --savedir lorenz63_loop_10000epochs_hs/iter3 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hs.py --savedir lorenz63_loop_10000epochs_hs/iter4 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hs.py --savedir lorenz63_loop_10000epochs_hs/iter5 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hs.py --savedir lorenz63_loop_10000epochs_hs/iter6 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hs.py --savedir lorenz63_loop_10000epochs_hs/iter7 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hs.py --savedir lorenz63_loop_10000epochs_hs/iter8 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hs.py --savedir lorenz63_loop_10000epochs_hs/iter9 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hiddensizes.py --savedir lorenz63_loop_10000epochs_hs/iter0 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hiddensizes.py --savedir lorenz63_loop_10000epochs_hs/iter1 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hiddensizes.py --savedir lorenz63_loop_10000epochs_hs/iter2 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hiddensizes.py --savedir lorenz63_loop_10000epochs_hs/iter3 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hiddensizes.py --savedir lorenz63_loop_10000epochs_hs/iter4 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hiddensizes.py --savedir lorenz63_loop_10000epochs_hs/iter5 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hiddensizes.py --savedir lorenz63_loop_10000epochs_hs/iter6 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hiddensizes.py --savedir lorenz63_loop_10000epochs_hs/iter7 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hiddensizes.py --savedir lorenz63_loop_10000epochs_hs/iter8 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hiddensizes.py --savedir lorenz63_loop_10000epochs_hs/iter9 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9
 wht
