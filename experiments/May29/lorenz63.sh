ee4e5cdbd6d274408f84762caa8726779e0b590f
Matt Levine <mattlevine22@gmail.com>
2019-05-29 18:44:33 -0700

    precompute test batch

---
+++ experiments/May29/lorenz63.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Submit this script with: sbatch train_submission
+
+#SBATCH --time=12:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --mem-per-cpu=25G   # memory per CPU core
+#SBATCH -c 1 # 1 core per task
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+module purge
+module load cuda/9.0
+module load python/2.7.15-tf
+
+# Execute jobs in parallel
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_testPreCompute.py --savedir lorenz63/test_preCompute --epoch 1000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+wait
