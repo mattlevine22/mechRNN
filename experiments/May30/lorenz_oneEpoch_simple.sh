b48edb3d9ec505496af942a7e00791240d8b0b41
Matt Levine <mattlevine22@gmail.com>
2019-05-31 15:18:10 -0700

    adding 2dOsc run scripts and batch script

--- experiments/May30/lorenz_oneEpoch_simple.sh
+++ experiments/May30/lorenz_oneEpoch_simple.sh
@@ -3,7 +3,7 @@
 #Submit this script with: sbatch train_submission
 
 #SBATCH --time=48:00:00   # walltime
-#SBATCH --ntasks=4   # number of processor cores (i.e. tasks)
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --mem-per-cpu=40G   # memory per CPU core
 #SBATCH -c 1 # 1 core per task
@@ -14,5 +14,5 @@ module load cuda/9.0
 module load python/2.7.15-tf
 
 # Execute jobs in parallel
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_simple.py --savedir lorenz63_v2/oneEpoch --epoch 1 --delta_t 0.1 --t_end 2000000 --train_frac 0.99999
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_simple.py --savedir lorenz63_v2/oneEpoch_print --epoch 1 --delta_t 0.1 --t_end 2000000 --train_frac 0.99999
 wait
