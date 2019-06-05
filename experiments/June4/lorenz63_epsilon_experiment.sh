e8b4bcae66c341e45e92e351adb6b1a337964574
Matt Levine <mattlevine22@gmail.com>
2019-06-05 00:41:12 -0700

    fewer runs

--- experiments/June4/lorenz63_epsilon_experiment.sh
+++ experiments/June4/lorenz63_epsilon_experiment.sh
@@ -3,9 +3,9 @@
 #Submit this script with: sbatch train_submission
 
 #SBATCH --time=48:00:00   # walltime
-#SBATCH --ntasks=10   # number of processor cores (i.e. tasks)
+#SBATCH --ntasks=3   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
-#SBATCH --mem-per-cpu=15   # memory per CPU core
+#SBATCH --mem-per-cpu=30   # memory per CPU core
 #SBATCH -c 1 # 1 core per task
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
@@ -16,12 +16,12 @@ module load python/2.7.15-tf
 # Execute jobs in parallel
 srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_eps.py --savedir lorenz63_eps_loop_10000epochs_DefaultInit/Iter0 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
 srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_eps.py --savedir lorenz63_eps_loop_10000epochs_DefaultInit/Iter1 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_eps.py --savedir lorenz63_eps_loop_10000epochs_DefaultInit/Iter2 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_eps.py --savedir lorenz63_eps_loop_10000epochs_DefaultInit/Iter3 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_eps.py --savedir lorenz63_eps_loop_10000epochs_DefaultInit/Iter4 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_eps.py --savedir lorenz63_eps_loop_10000epochs_DefaultInit/Iter5 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_eps.py --savedir lorenz63_eps_loop_10000epochs_DefaultInit/Iter6 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_eps.py --savedir lorenz63_eps_loop_10000epochs_DefaultInit/Iter7 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_eps.py --savedir lorenz63_eps_loop_10000epochs_DefaultInit/Iter8 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_eps.py --savedir lorenz63_eps_loop_10000epochs_DefaultInit/Iter9 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_eps.py --savedir lorenz63_eps_loop_10000epochs_DefaultInit/Iter2 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9
+# srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_eps.py --savedir lorenz63_eps_loop_10000epochs_DefaultInit/Iter3 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+# srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_eps.py --savedir lorenz63_eps_loop_10000epochs_DefaultInit/Iter4 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+# srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_eps.py --savedir lorenz63_eps_loop_10000epochs_DefaultInit/Iter5 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+# srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_eps.py --savedir lorenz63_eps_loop_10000epochs_DefaultInit/Iter6 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+# srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_eps.py --savedir lorenz63_eps_loop_10000epochs_DefaultInit/Iter7 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+# srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_eps.py --savedir lorenz63_eps_loop_10000epochs_DefaultInit/Iter8 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9 &
+# srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_eps.py --savedir lorenz63_eps_loop_10000epochs_DefaultInit/Iter9 --epoch 10000 --delta_t 0.1 --t_end 100 --train_frac 0.9
 wait
