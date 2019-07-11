12578a6b52341c2b3d5d6910501eb23e2a4b0f52
Matt Levine <mattlevine22@gmail.com>
2019-07-11 01:05:51 -0700

    no KL in hidden sizes experiment

--- experiments/July9/lorenz63_hidden_size_experiment_manyInits.sh
+++ experiments/July9/lorenz63_hidden_size_experiment_manyInits.sh
@@ -14,14 +14,14 @@ module load cuda/9.0
 module load python/2.7.15-tf
 
 # Execute jobs in parallel
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hiddensizes.py --savedir lorenz63_hiddensizes/Init0 --epoch 10000 --delta_t 0.1 --t_end 300 --train_frac 0.333 --random_state_inits True --compute_kl True &
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hiddensizes.py --savedir lorenz63_hiddensizes/Init1 --epoch 10000 --delta_t 0.1 --t_end 300 --train_frac 0.333 --random_state_inits True --compute_kl True &
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hiddensizes.py --savedir lorenz63_hiddensizes/Init2 --epoch 10000 --delta_t 0.1 --t_end 300 --train_frac 0.333 --random_state_inits True --compute_kl True &
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hiddensizes.py --savedir lorenz63_hiddensizes/Init3 --epoch 10000 --delta_t 0.1 --t_end 300 --train_frac 0.333 --random_state_inits True --compute_kl True &
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hiddensizes.py --savedir lorenz63_hiddensizes/Init4 --epoch 10000 --delta_t 0.1 --t_end 300 --train_frac 0.333 --random_state_inits True --compute_kl True &
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hiddensizes.py --savedir lorenz63_hiddensizes/Init5 --epoch 10000 --delta_t 0.1 --t_end 300 --train_frac 0.333 --random_state_inits True --compute_kl True &
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hiddensizes.py --savedir lorenz63_hiddensizes/Init6 --epoch 10000 --delta_t 0.1 --t_end 300 --train_frac 0.333 --random_state_inits True --compute_kl True &
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hiddensizes.py --savedir lorenz63_hiddensizes/Init7 --epoch 10000 --delta_t 0.1 --t_end 300 --train_frac 0.333 --random_state_inits True --compute_kl True &
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hiddensizes.py --savedir lorenz63_hiddensizes/Init8 --epoch 10000 --delta_t 0.1 --t_end 300 --train_frac 0.333 --random_state_inits True --compute_kl True &
-srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hiddensizes.py --savedir lorenz63_hiddensizes/Init9 --epoch 10000 --delta_t 0.1 --t_end 300 --train_frac 0.333 --random_state_inits True --compute_kl True
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hiddensizes.py --savedir lorenz63_hiddensizes/Init0 --epoch 10000 --delta_t 0.1 --t_end 300 --train_frac 0.333 --random_state_inits True --compute_kl False &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hiddensizes.py --savedir lorenz63_hiddensizes/Init1 --epoch 10000 --delta_t 0.1 --t_end 300 --train_frac 0.333 --random_state_inits True --compute_kl False &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hiddensizes.py --savedir lorenz63_hiddensizes/Init2 --epoch 10000 --delta_t 0.1 --t_end 300 --train_frac 0.333 --random_state_inits True --compute_kl False &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hiddensizes.py --savedir lorenz63_hiddensizes/Init3 --epoch 10000 --delta_t 0.1 --t_end 300 --train_frac 0.333 --random_state_inits True --compute_kl False &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hiddensizes.py --savedir lorenz63_hiddensizes/Init4 --epoch 10000 --delta_t 0.1 --t_end 300 --train_frac 0.333 --random_state_inits True --compute_kl False &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hiddensizes.py --savedir lorenz63_hiddensizes/Init5 --epoch 10000 --delta_t 0.1 --t_end 300 --train_frac 0.333 --random_state_inits True --compute_kl False &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hiddensizes.py --savedir lorenz63_hiddensizes/Init6 --epoch 10000 --delta_t 0.1 --t_end 300 --train_frac 0.333 --random_state_inits True --compute_kl False &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hiddensizes.py --savedir lorenz63_hiddensizes/Init7 --epoch 10000 --delta_t 0.1 --t_end 300 --train_frac 0.333 --random_state_inits True --compute_kl False &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hiddensizes.py --savedir lorenz63_hiddensizes/Init8 --epoch 10000 --delta_t 0.1 --t_end 300 --train_frac 0.333 --random_state_inits True --compute_kl False &
+srun --exclusive -N 1 -n 1 python ../scripts/run_script_lorenz63_SCRIPTSTYLE_hiddensizes.py --savedir lorenz63_hiddensizes/Init9 --epoch 10000 --delta_t 0.1 --t_end 300 --train_frac 0.333 --random_state_inits True --compute_kl False
 wait
