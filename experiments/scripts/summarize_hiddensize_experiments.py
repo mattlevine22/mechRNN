12275fed113fd3d7bb0498059b0a5da7bd451261
Matt Levine <mattlevine22@gmail.com>
2019-08-14 13:01:43 +0200

    new performance extractor

--- experiments/scripts/summarize_hiddensize_experiments.py
+++ experiments/scripts/summarize_hiddensize_experiments.py
@@ -9,7 +9,8 @@ def main(pathname):
 		x = str(x)
 		if os.path.exists(x+'/loss_vec_clean_test.txt'):
 			my_dirs.append(x)
-	extract_hidden_size_performance(my_dirs,output_fname=pathname+'/compare_hidden_sizes')
+	# extract_hidden_size_performance(my_dirs,output_fname=pathname+'/compare_hidden_sizes')
+	extract_performance1(my_dirs, output_fname=pathname+'/compare_hidden_sizes', win=1, hs_token='hs', n_gprs=0, my_labels={'xlabel': 'Dimension of Hidden State'})
 
 if __name__ == '__main__':
 	main(sys.argv[1])
