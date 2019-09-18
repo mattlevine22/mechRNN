51e7827c8364d4be175665f5411f47fe6c642d5c
Matt Levine <mattlevine22@gmail.com>
2019-09-17 17:11:25 -0700

    add output_dir name

--- experiments/Sep11/run_summary.sh
+++ experiments/Sep11/run_summary.sh
@@ -5,4 +5,4 @@ module purge
 module load cuda/9.0
 module load python/2.7.15-tf
 
-python ../scripts/lorenz63_3dvar_summarize.py --n_train_trajectories 10 --n_test_trajectories 10
+python ../scripts/lorenz63_3dvar_summarize.py --output_dir 3DVAR_learning_V1 --n_train_trajectories 10 --n_test_trajectories 10
