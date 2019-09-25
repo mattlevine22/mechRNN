13e76706a1d6dc62b2c5e4e1d93d09c3b32a8cef
Matt Levine <mattlevine22@gmail.com>
2019-09-25 14:54:15 -0700

    test shorter run

--- experiments/Sep11/run_data_generation_no_cheating.sh
+++ experiments/Sep11/run_data_generation_no_cheating.sh
@@ -5,5 +5,5 @@ module purge
 module load cuda/9.0
 module load python/2.7.15-tf
 
-python ../scripts/lorenz63_3dvar_generateData.py --n_trajectories 10 --output_dir /groups/astuart/mlevine/Sep11/3DVAR_learning_no_cheat_V2_50 --output_filename TrainData.npz --t_end 2000
+python ../scripts/lorenz63_3dvar_generateData.py --n_trajectories 10 --output_dir /groups/astuart/mlevine/Sep11/3DVAR_learning_no_cheat_V2_50 --output_filename TrainData.npz --t_end 50
 python ../scripts/lorenz63_3dvar_generateData.py --n_trajectories 10 --output_dir /groups/astuart/mlevine/Sep11/3DVAR_learning_no_cheat_V2_50 --output_filename TestData.npz --t_end 5
