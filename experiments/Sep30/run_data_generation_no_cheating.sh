bb09c6f3e19227654e790667c31fc9ed79a1ecc8
Matt Levine <mattlevine22@gmail.com>
2019-09-30 12:32:54 -0700

    adding new sweep run

---
+++ experiments/Sep30/run_data_generation_no_cheating.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+module purge
+module load cuda/9.0
+module load python/2.7.15-tf
+
+python ../scripts/lorenz63_3dvar_generateData.py --n_trajectories 10 --output_dir /groups/astuart/mlevine/Sep30/3DVAR_learning_no_cheat_50_sweep1 --output_filename TrainData.npz --t_end 50
+python ../scripts/lorenz63_3dvar_generateData.py --n_trajectories 10 --output_dir /groups/astuart/mlevine/Sep30/3DVAR_learning_no_cheat_50_sweep1 --output_filename TestData.npz --t_end 5
+
---
+++ experiments/Sep30/run_summary_no_cheating.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+module purge
+module load cuda/9.0
+module load python/2.7.15-tf
+
+python ../scripts/lorenz63_3dvar_summarize.py --output_dir /groups/astuart/mlevine/Sep30/3DVAR_learning_no_cheat_50_sweep1 --n_train_trajectories 10 --n_test_trajectories 10
