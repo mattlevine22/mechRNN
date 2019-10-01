992aff9e2ee1eb049d5ebffd277e62bed8e50606
Matt Levine <mattlevine22@gmail.com>
2019-10-01 12:12:23 -0700

    only do 500 long run

--- experiments/Sep30/run_data_generation_no_cheating.sh
+++ experiments/Sep30/run_data_generation_no_cheating.sh
@@ -5,6 +5,6 @@ module purge
 module load cuda/9.0
 module load python/2.7.15-tf
 
-python ../scripts/lorenz63_3dvar_generateData.py --n_trajectories 10 --output_dir /groups/astuart/mlevine/Sep30/3DVAR_learning_no_cheat_2000_sweep2_smallInits_100Qtries --output_filename TrainData.npz --t_end 2000
-python ../scripts/lorenz63_3dvar_generateData.py --n_trajectories 10 --output_dir /groups/astuart/mlevine/Sep30/3DVAR_learning_no_cheat_2000_sweep2_smallInits_100Qtries --output_filename TestData.npz --t_end 20
+python ../scripts/lorenz63_3dvar_generateData.py --n_trajectories 10 --output_dir /groups/astuart/mlevine/Sep30/3DVAR_learning_no_cheat_500_sweep2_smallInits_100Qtries --output_filename TrainData.npz --t_end 500
+python ../scripts/lorenz63_3dvar_generateData.py --n_trajectories 10 --output_dir /groups/astuart/mlevine/Sep30/3DVAR_learning_no_cheat_500_sweep2_smallInits_100Qtries --output_filename TestData.npz --t_end 20
 
