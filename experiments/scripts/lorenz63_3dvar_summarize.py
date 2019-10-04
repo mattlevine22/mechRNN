1d4bf63283f561f84ac4bb582efde1db2bc58c8a
Matt Levine <mattlevine22@gmail.com>
2019-10-04 16:04:21 -0700

    more epsilon plotting

--- experiments/scripts/lorenz63_3dvar_summarize.py
+++ experiments/scripts/lorenz63_3dvar_summarize.py
@@ -334,7 +334,6 @@ def epsilon_summary(my_dirs=None, output_dir='default_output', n_train_trajector
 	m = -1
 	for method_nm in G_all:
 		m += 1
-		pdb.set_trace()
 		for kk in range(G.shape[1]):
 			mean_vec = [G_all[method_nm][eps_val]['mean'][kk] for eps_val in eps_vec]
 			std_vec = [G_all[method_nm][eps_val]['std'][kk] for eps_val in eps_vec]
