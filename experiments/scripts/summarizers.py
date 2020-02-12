edeac82f0eab94d2cedecc4c4cb0de2ca679fe97
Matt Levine <mattlevine22@gmail.com>
2020-02-12 12:11:05 -0800

    fix new summarizer

--- experiments/scripts/summarizers.py
+++ experiments/scripts/summarizers.py
@@ -159,7 +159,7 @@ def extract_epsilon_performance(my_dirs, output_fname="./epsilon_comparisons", w
 					eps_vec = sorted(method_summary[method_nm][is_resid][is_flow][metric_nm]['median'].keys())
 					median_vec = [method_summary[method_nm][is_resid][is_flow][metric_nm]['median'][eps] for eps in eps_vec]
 					std_vec = [method_summary[method_nm][is_resid][is_flow][metric_nm]['std'][eps] for eps in eps_vec]
-					axlist[c].errorbar(x=eps_vec, y=median_vec, yerr=std_vec, label=nm, linestyle=linestyle, color=color, marker=marker)
+					axlist[c].errorbar(x=eps_vec, y=median_vec, yerr=std_vec, label=nm, linestyle=linestyle, color=color, marker=marker, fillstyle='none')
 
 
 	axlist[0].set_xlabel('epsilon Model Error')
