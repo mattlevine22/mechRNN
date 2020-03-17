73984d897f060ebc70de8a6061be452794408085
Matt Levine <mattlevine22@gmail.com>
2020-03-16 22:05:13 -0700

    forgot to add function

---
+++ experiments/scripts/generate_data_wrapper.py
@@ -0,0 +1,22 @@
+import argparse
+from utils import generate_data
+from pydoc import locate
+
+parser = argparse.ArgumentParser(description='mechRNN')
+parser.add_argument('--settings_path', type=str, default='datagen_settings.npz', help='pathname of numpy settings dictionary')
+FLAGS = parser.parse_args()
+
+def main(settings_path=FLAGS.settings_path):
+	with open(settings_path) as f:
+	  setts = json.load(f)
+	# https://stackoverflow.com/questions/547829/how-to-dynamically-load-a-python-class
+	my_class = locate('odelibrary.{0}'.format(setts['odeclass']))
+	my_class = my_class(**setts['param_dict'])
+	setts['ODE'] = my_class(**setts['param_dict'])
+	# setts['f_get_inits'] = getattr(my_class, 'get_inits')
+	generate_data(**setts)
+	return
+
+if __name__ == '__main__':
+	main()
+
