98c0f9b885cba6b432f029e1281339ee74ce4136
Matt Levine <mattlevine22@gmail.com>
2020-03-18 12:37:52 -0700

    bugfix

--- experiments/scripts/odelibrary.py
+++ experiments/scripts/odelibrary.py
@@ -70,9 +70,9 @@ class L96M:
 
   def plot_state_indices(_s):
     if _s.slow_only:
-      return [0, 1, K-1, K-2] # return a 4 coupled slow variables
+      return [0, 1, _s.K-1, _s.K-2] # return a 4 coupled slow variables
     else:
-      return [0, K] # return 1st slow variable and 1st coupled fast variable
+      return [0, _s.K] # return 1st slow variable and 1st coupled fast variable
 
   def set_predictor(_s, predictor):
     _s.predictor = predictor
