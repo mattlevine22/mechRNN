0edd3d7730351d7637863684c075dfb22c08954d
Matt Levine <mattlevine22@gmail.com>
2020-03-25 14:10:51 -0700

    troubleshoot

--- experiments/scripts/odelibrary.py
+++ experiments/scripts/odelibrary.py
@@ -355,6 +355,7 @@ class L96M:
     return Ybar
 
   def implied_Ybar(_s, X, delta_t):
+    pdb.set_trace()
     Ybar = np.zeros( (X.shape[0], _s.K*_s.J) )
     for j in range(X.shape[0]-1):
       Ybar[j,:] = _s.single_step_implied_Ybar(Xnow=X[j,:], Xnext=X[j+1,:], delta_t=delta_t)
