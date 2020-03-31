818aef99fc02f86e46cb91c16f8e27026584f5f0
Matt Levine <mattlevine22@gmail.com>
2020-03-31 16:06:35 -0700

    fixed the bug

--- experiments/scripts/utils.py
+++ experiments/scripts/utils.py
@@ -1100,8 +1100,8 @@ def run_ode_test(y_clean_test, y_noisy_test,
 
         # plot inferred Ybar vs true Ybar
         if y_fast_test is not None:
-            pdb.set_trace()
             X_in = np.vstack((y_noisy_testSynch[kkt,-1,:], y_clean_test[kkt,:-1,:]))
+            X_in = f_unNormalize_Y(normz_info,X_in)
             X_out = gpr_test_predictions_onestep_raw
             Ybar_inferred = ODE.implied_Ybar(X_in=X_in, X_out=X_out, delta_t=model_params['delta_t'])
             Ybar_true = y_fast_test[kkt,:,:].reshape( (y_fast_test.shape[1], ODE.J, ODE.K), order = 'F').sum(axis = 1) / ODE.J
