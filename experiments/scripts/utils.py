33841f79a353d886e7e15b77941600d28a173530
Matt Levine <mattlevine22@gmail.com>
2020-04-03 10:34:50 -0700

    fix data type

--- experiments/scripts/utils.py
+++ experiments/scripts/utils.py
@@ -1198,7 +1198,7 @@ def run_GP(y_clean_train, y_noisy_train,
             y_fast_train=None,
             ODE=None,
             gp_space_map='fulltofull',
-            n_subsample=1e4):
+            n_subsample=int(1e4)):
 
     y_noisy_train_raw = f_unNormalize_Y(normz_info,y_noisy_train)
     y_clean_train_raw = f_unNormalize_Y(normz_info,y_clean_train)
