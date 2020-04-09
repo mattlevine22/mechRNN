44b319286d66f70d7a11093de32c2b405017cd86
Matt Levine <mattlevine22@gmail.com>
2020-04-09 16:41:16 -0700

    improve gp_closure plotter

--- experiments/scripts/gp_closure_plotting.py
+++ experiments/scripts/gp_closure_plotting.py
@@ -78,6 +78,8 @@ def main(basedir=FLAGS.basedir,
 
 			# fit GP to all states together
 			Xtrain = X.reshape(-1, 1)
+			Xmin = np.min(Xtrain)
+			Xmax = np.max(Xtrain)
 			if infer_Ybar:
 				ytrain = Ybar_data_inferred.reshape(-1, 1)
 			else:
