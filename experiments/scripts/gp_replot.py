8c5118c76ebfdc4336d72be12cff0596efd6a6de
Matt Levine <mattlevine22@gmail.com>
2020-04-09 19:02:56 -0700

    plotting descriptive Xk vs Ybark for GPs (discrete and continuous)

--- experiments/scripts/gp_replot.py
+++ experiments/scripts/gp_replot.py
@@ -44,7 +44,8 @@ def plot_gp_x_vs_y(gp_path, data_path, infer_Ybar=False):
 	X_min = np.min(Xtrain)
 	X_max = np.max(Xtrain)
 	X_k_pred = np.arange(X_min,X_max,0.01).reshape(-1, 1)
-	gpr_list = pickle.load(gp_path)
+	pdb.set_trace()
+	gpr_list = pickle.loads(gp_path)
 	for gpr in gpr_list:
 		gp_mean, gp_std = gpr.predict(X_k_pred, return_std=True)
 		ax_mean.plot(X_k_pred, gp_mean, color='black', linestyle='-')
