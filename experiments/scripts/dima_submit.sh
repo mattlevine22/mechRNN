32870cc3c08ee8789ed706e1fc25d172d7bbe2b2
Matt Levine <mattlevine22@gmail.com>
2020-04-10 16:29:31 -0700

    adding dima reproduction stuff

--- experiments/scripts/dima_submit.sh
+++ experiments/scripts/dima_submit.sh
@@ -9,8 +9,8 @@ module purge
 module load python3/3.7.0
 
 # Execute jobs in parallel
-srun --exclusive -N 1 -n 1 python3 reproduce_dima.py --output_dir /groups/astuart/mlevine/writeup0/l96_dt_trials_v2/dt0.001/F50_eps0.0078125/reproduce_dima_tSynch50_tInvMeas100_nGpSub800 --t_synch 50 --t_train 10 --t_invariant_measure 100 --n_subsample_gp 800 &
-srun --exclusive -N 1 -n 1 python3 reproduce_dima.py --output_dir /groups/astuart/mlevine/writeup0/l96_dt_trials_v2/dt0.001/F50_eps0.0078125/reproduce_dima_tSynch500_tInvMeas100_nGpSub800 --t_synch 500 --t_train 10 --t_invariant_measure 100 --n_subsample_gp 800 &
-srun --exclusive -N 1 -n 1 python3 reproduce_dima.py --output_dir /groups/astuart/mlevine/writeup0/l96_dt_trials_v2/dt0.001/F50_eps0.0078125/reproduce_dima_tSynch500_tInvMeas2000_nGpSub800 --t_synch 500 --t_train 10 --t_invariant_measure 2000 --n_subsample_gp 800 &
-srun --exclusive -N 1 -n 1 python3 reproduce_dima.py --output_dir /groups/astuart/mlevine/writeup0/l96_dt_trials_v2/dt0.001/F50_eps0.0078125/reproduce_dima_tSynch500_tInvMeas1000_nGpSub800 --t_synch 500 --t_train 10 --t_invariant_measure 2000 --n_subsample_gp 5000 &
+srun --exclusive -N 1 -n 1 python3 reproduce_dima.py --output_dir /groups/astuart/mlevine/writeup0/l96_dt_trials_v2/dt0.001/F50_eps0.0078125/reproduce_dima_K4_J4_tSynch50_tInvMeas100_nGpSub800 --K 4 --J 4 --F 50 --t_synch 50 --t_train 10 --t_invariant_measure 100 --n_subsample_gp 800 &
+srun --exclusive -N 1 -n 1 python3 reproduce_dima.py --output_dir /groups/astuart/mlevine/writeup0/l96_dt_trials_v2/dt0.001/F50_eps0.0078125/reproduce_dima_K4_J4_tSynch500_tInvMeas100_nGpSub800 --K 4 --J 4 --F 50 --t_synch 500 --t_train 10 --t_invariant_measure 100 --n_subsample_gp 800 &
+srun --exclusive -N 1 -n 1 python3 reproduce_dima.py --output_dir /groups/astuart/mlevine/writeup0/l96_dt_trials_v2/dt0.001/F50_eps0.0078125/reproduce_dima_K4_J4_tSynch500_tInvMeas2000_nGpSub800 --K 4 --J 4 --F 50 --t_synch 500 --t_train 10 --t_invariant_measure 2000 --n_subsample_gp 800 &
+srun --exclusive -N 1 -n 1 python3 reproduce_dima.py --output_dir /groups/astuart/mlevine/writeup0/l96_dt_trials_v2/dt0.001/F50_eps0.0078125/reproduce_dima_K4_J4_tSynch500_tInvMeas1000_nGpSub800 --K 4 --J 4 --F 50 --t_synch 500 --t_train 10 --t_invariant_measure 2000 --n_subsample_gp 5000
 wait
