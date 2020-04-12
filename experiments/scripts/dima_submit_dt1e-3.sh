815e10053f86aa10741f9fc51379de4e6f2f87a9
Matt Levine <mattlevine22@gmail.com>
2020-04-12 13:32:02 -0700

    adding dima reproduction stuff

---
+++ experiments/scripts/dima_submit_dt1e-3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --time=6:00:00   # walltime
+#SBATCH --ntasks=10   # number of processor cores (i.e. tasks)
+#SBATCH -c 1 # 1 core per task
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+module purge
+module load python3/3.7.0
+
+# Execute jobs in parallel
+srun --exclusive -N 1 -n 1 python3 reproduce_dima.py --output_dir /groups/astuart/mlevine/writeup0/reproduce_dima/dt0.001/K4_J4_tSynch50_tInvMeas100_nGpSub800 --delta_t 0.001 --K 4 --J 4 --F 50 --t_synch 50 --t_train 10 --t_invariant_measure 100 --n_subsample_gp 800 &
+srun --exclusive -N 1 -n 1 python3 reproduce_dima.py --output_dir /groups/astuart/mlevine/writeup0/reproduce_dima/dt0.001/K4_J4_tSynch500_tInvMeas100_nGpSub800 --delta_t 0.001 --K 4 --J 4 --F 50 --t_synch 500 --t_train 10 --t_invariant_measure 100 --n_subsample_gp 800 &
+srun --exclusive -N 1 -n 1 python3 reproduce_dima.py --output_dir /groups/astuart/mlevine/writeup0/reproduce_dima/dt0.001/K4_J4_tSynch500_tInvMeas2000_nGpSub800 --delta_t 0.001 --K 4 --J 4 --F 50 --t_synch 500 --t_train 10 --t_invariant_measure 2000 --n_subsample_gp 800 &
+srun --exclusive -N 1 -n 1 python3 reproduce_dima.py --output_dir /groups/astuart/mlevine/writeup0/reproduce_dima/dt0.001/K4_J4_tSynch500_tInvMeas2000_nGpSub1600 --delta_t 0.001 --K 4 --J 4 --F 50 --t_synch 500 --t_train 10 --t_invariant_measure 2000 --n_subsample_gp 1600
+srun --exclusive -N 1 -n 1 python3 reproduce_dima.py --output_dir /groups/astuart/mlevine/writeup0/reproduce_dima/dt0.001/tSynch50_tInvMeas100_nGpSub800 --delta_t 0.001 --K 9 --J 8 --F 10 --t_synch 50 --t_train 10 --t_invariant_measure 100 --n_subsample_gp 800 &
+srun --exclusive -N 1 -n 1 python3 reproduce_dima.py --output_dir /groups/astuart/mlevine/writeup0/reproduce_dima/dt0.001/tSynch500_tInvMeas100_nGpSub800 --delta_t 0.001 --K 9 --J 8 --F 10 --t_synch 500 --t_train 10 --t_invariant_measure 100 --n_subsample_gp 800 &
+srun --exclusive -N 1 -n 1 python3 reproduce_dima.py --output_dir /groups/astuart/mlevine/writeup0/reproduce_dima/dt0.001/tSynch500_tInvMeas2000_nGpSub800 --delta_t 0.001 --K 9 --J 8 --F 10 --t_synch 500 --t_train 10 --t_invariant_measure 2000 --n_subsample_gp 800 &
+srun --exclusive -N 1 -n 1 python3 reproduce_dima.py --output_dir /groups/astuart/mlevine/writeup0/reproduce_dima/dt0.001/tSynch500_tInvMeas2000_nGpSub1600 --delta_t 0.001 --K 9 --J 8 --F 10 --t_synch 500 --t_train 10 --t_invariant_measure 2000 --n_subsample_gp 1600
+wait
